4b4caea7fea7770bb7a2a3afe86a011d31109a6a
Wojciech Siekiera <wojciech@siekiera.net>
2021-12-12 23:19:42 +0100

    Initial commit

---
+++ src/main/java/net/siekiera/springinabox/SpringinaboxApplication.java
@@ -0,0 +1,13 @@
+package net.siekiera.springinabox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringinaboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringinaboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/siekiera/springinabox/SpringinaboxApplicationTests.java
@@ -0,0 +1,13 @@
+package net.siekiera.springinabox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringinaboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
